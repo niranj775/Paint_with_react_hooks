0c7daa24fb8f2772b965ecb4f9df40823ab11400
niranj775 <niranj775@gmail.com>
2020-07-24 21:46:44 +0530

    color

--- src/Name.js
+++ src/Name.js
@@ -4,7 +4,7 @@ import './Name.css'
 function Name(){
     const [name, setName] = useState('')
     return(
-        <label>
+        <label className="header-name">
             <input 
             value={name}
             onChange={ e => setName(e.target.value) }
--- src/Playground.js
+++ src/Playground.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react'
-// import randomColor from 'randomcolor'
+import React, { useState, useEffect } from 'react'
+import randomColor from 'randomcolor'
 import './App.css'
 
-function App() {
+function Playground() {
   const [count, setCount] = useState(0)
+
+  const [color, setColor] = useState(null)
+  useEffect( () => {
+    setColor(randomColor())
+  },[count])
   
   return (
-    <div>
+    <div style={{ borderTop: `10px solid ${color}` }}>
       {count}
       <button onClick={() => setCount(currentCount => currentCount + 1)} >+</button>  
       <button onClick={() => setCount(currentCount => currentCount - 1)}>-</button>  
@@ -14,4 +19,4 @@ function App() {
   );
 }
 
-export default App;
+export default Playground;
