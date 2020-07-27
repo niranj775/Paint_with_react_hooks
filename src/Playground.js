95e676a0844cda2da026a0bf76fb1a641d098153
niranj775 <niranj775@gmail.com>
2020-07-27 09:16:02 +0530

    useRef Eg

--- src/Playground.js
+++ src/Playground.js
@@ -1,22 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import randomColor from 'randomcolor'
-import './App.css'
 
-function Playground() {
-  const [count, setCount] = useState(0)
-
+export default function Playground() {
+  const [count, setCount] = useState(30)
+  
+  const inputRef = useRef()
+  
   const [color, setColor] = useState(null)
-  useEffect( () => {
+  useEffect(() => {
     setColor(randomColor())
-  },[count])
+    inputRef.current.focus()
+  }, [count])
   
   return (
-    <div style={{ borderTop: `10px solid ${color}` }}>
+    <div style={{ borderTop: `10px solid ${color}`}}>
       {count}
-      <button onClick={() => setCount(currentCount => currentCount + 1)} >+</button>  
-      <button onClick={() => setCount(currentCount => currentCount - 1)}>-</button>  
+      <button onClick={() => setCount(currentCount => currentCount - 1)}>-</button>
+      <button onClick={() => setCount(currentCount => currentCount + 1)}>+</button>
+      <hr />
+      <input ref={inputRef} type="range" onChange={e => setCount(e.target.value)} value={count} />
     </div>
-  );
+  )
 }
-
-export default Playground;
